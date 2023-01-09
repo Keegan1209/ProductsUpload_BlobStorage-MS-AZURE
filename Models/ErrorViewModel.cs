f7d3c4d24beff39d9cd926fd6cadf25a479f69e5
User <User@KEEGANSPC>
2023-01-09 22:25:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POE2_CLDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
