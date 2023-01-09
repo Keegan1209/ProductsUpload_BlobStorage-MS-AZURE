a375933ead0a403f879d0f67fac4164b4e7adfc0
Keegan Frank <66334754+Keegan1209@users.noreply.github.com>
2023-01-09 22:29:37 +0200

    Add files via upload

---
+++ ClassLibraryCLDV/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ClassLibraryCLDV.AppConfig
+{
+    public static class Class1
+    {
+        private static IConfiguration currentConfig;
+
+        public static void SetConfig(IConfiguration configuration)
+        {
+            currentConfig = configuration;
+        }
+
+
+        public static string GetConfiguration(string configKey)
+        {
+            try
+            {
+                string connectionString = currentConfig.GetConnectionString(configKey);
+                return connectionString;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            return "";
+        }
+
+    }
+}
---
+++ ClassLibraryCLDV/products.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace ClassLibraryCLDV.Models
+{
+    [Table ("Product", Schema = "Core")]
+    public class products
+    {
+       
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name")]
+        [Column(TypeName = "varchar(50)")]
+        public string PName { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Description")]
+        [Column(TypeName = "varchar(500)")]
+        public string PDescription { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string ImageName { get; set; }
+
+        [Column(TypeName = "varchar(250)")]
+        public string ImagePath { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        [NotMapped]
+        public IFormFile File { get; set; }
+
+    }
+}
